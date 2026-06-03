6bb2ba5b1424d2e0da65c0644925bca43cd743fe
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-06-03 21:56:33 +0700

    refactor: update config and unify collections (Nanoo-fied)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,12 @@
-// @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config"
+import node from "@astrojs/node"
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-webrings.lou.gg',
-
-	experimental: {
-		fonts: [
-			{
-				cssVariable: '--font-inter',
-				name: 'Inter',
-				provider: fontProviders.google(),
-				fallbacks: ['Helvetica', 'sans-serif'],
-				display: 'swap',
-				subsets: ['latin'],
-				weights: [400, 700],
-			},
-		],
-	},
-
-	adapter: node({
-		mode: 'standalone',
-	}),
-});
+  site: "https://webrings.nanoolabs.dev",
+  image: {
+    service: passthroughImageService(),
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
+})
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,43 +1,20 @@
-import { defineCollection, z } from 'astro:content'
-import { glob, file } from 'astro/loaders'
+import { defineCollection, z } from "astro:content"
+import { glob } from "astro/loaders"
 
 export const webringSchema = z.object({
-	name: z.string().min(1, { message: 'Site name has to be at least one character long!' }),
-	id: z
-		.string()
-		.min(1, { message: 'Site slug has to be at least one character long!' })
-		.refine((slug) => {
-			const slugMatcher = /[a-z0-9-_.]/
-
-			if (slugMatcher.test(slug)) return true
-
-			return 'Slug must be in all lowercase and include only numbers, letters from a-z, hyphens, dots and underscores.'
-		}),
-	url: z.string().url({ message: 'Site url must be valid' }),
+  name: z.string().min(1),
+  id: z.string().min(1),
+  url: z.string().url(),
 })
 
-const webring_friendsOfHouston = defineCollection({
-	loader: glob({ pattern: '*.json', base: './src/content/rings/friends-of-houston' }),
-	schema: webringSchema,
-})
-
-const webring_builtWithAstro = defineCollection({
-	loader: glob({ pattern: '*.json', base: './src/content/rings/built-with-astro' }),
-	schema: webringSchema,
-})
-
-const webrings = defineCollection({
-	loader: file('src/content/rings/rings.json'),
-	schema: z.object({
-		collection: z
-			.string()
-			.min(1, { message: 'Webring collection name needs to be at least one character long!' }),
-		slug: z.string().min(1, { message: 'Webring slug needs to be at least one character long!' }),
-	}),
+const rings = defineCollection({
+  loader: glob({
+    pattern: "**/*.json",
+    base: "./src/content/rings",
+  }),
+  schema: webringSchema,
 })
 
 export const collections = {
-	webrings,
-	webring_builtWithAstro,
-	webring_friendsOfHouston,
+  rings,
 }
