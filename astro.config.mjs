efee7518b1ce464251744be09b0e082716fdd449
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-06-04 18:33:14 +0700

    chore: migrate from Netlify to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig, passthroughImageService } from "astro/config"
-import netlify from "@astrojs/netlify"
+import vercel from "@astrojs/vercel"
 
 export default defineConfig({
   site: "https://webrings.nanoolabs.dev",
   image: {
-  // just using passthroughImageService when development on my termux
     service: process.env.NODE_ENV === "development" ? passthroughImageService() : undefined,
   },
-  // use netlify adapter only when on netlify to avoid deno issues in termux
-  adapter: process.env.NETLIFY === "true" ? netlify() : undefined,
+  // vercel handle the adapter automatically in production
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   output: "server",
 })
